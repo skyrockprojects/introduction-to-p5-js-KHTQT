0e7dfc7ad86082f05e6ad816fd49c6c92403d106
KHTQT <35992375+KHTQT@users.noreply.github.com>
2018-01-31 17:55:30 +0800

    Drawing shapes

--- first-p5-project/sketch.js
+++ first-p5-project/sketch.js
@@ -1,7 +1,16 @@
 function setup(){
-
+createCanvas(300, 600,);
+background(176,232,12);
+stroke(255);
+    fill(100);
+rect(100,150,100,50);
+fill(255,0,0);
+ellipse(100,20,200,40);
+stroke(0,255,0);
+line(20, 50, 100, 250);
 }
 
 function draw(){
 
 }
+
