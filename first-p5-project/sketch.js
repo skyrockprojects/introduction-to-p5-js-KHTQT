c8ae5559ba6bddc1b7678a16f32b6991eee6157e
KHTQT <35992375+KHTQT@users.noreply.github.com>
2018-02-07 18:03:25 +0800

    Smiley Face And Circle

--- first-p5-project/sketch.js
+++ first-p5-project/sketch.js
@@ -1,16 +1,28 @@
-function setup(){
-createCanvas(300, 600,);
-background(176,232,12);
-stroke(255);
-    fill(100);
-rect(100,150,100,50);
-fill(255,0,0);
-ellipse(100,20,200,40);
-stroke(0,255,0);
-line(20, 50, 100, 250);
-}
+   
+    /****SMILEY FACE****/
+    /*
+    function setup() {
+    createCanvas(300, 300);
+    background(255,206,0);
+    }
 
-function draw(){
+    point(100,200);
+    stroke(0,0,0);
+    fill(0,0,0);
+    ellipse(70,70,100,100);
+    stroke(0,0,0);
+    fill(0,0,0);
+    ellipse(220,70,100,100);
+    stroke(0,0,0);
+    fill(0,0,0);
+    rect(50,200,200,5);
+    */
 
-}
+    
+   var radius,r;
 
+   function setup() {
+    createCanvas(500, 500);
+    background(255)
+    radius = 0;
+    r = 600
