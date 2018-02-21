f6522881eb953dd4c12399f9747f79adc02b8200
KHTQT <35992375+KHTQT@users.noreply.github.com>
2018-02-21 18:04:58 +0800

    all files

--- first-p5-project/sketch.js
+++ first-p5-project/sketch.js
@@ -1,28 +1,42 @@
-   
-    /****SMILEY FACE****/
-    /*
-    function setup() {
-    createCanvas(300, 300);
-    background(255,206,0);
-    }
+/*function setup(){
 
-    point(100,200);
-    stroke(0,0,0);
-    fill(0,0,0);
-    ellipse(70,70,100,100);
-    stroke(0,0,0);
-    fill(0,0,0);
-    ellipse(220,70,100,100);
-    stroke(0,0,0);
-    fill(0,0,0);
-    rect(50,200,200,5);
-    */
+    createCanvas(800,600);
+    background(255,13,255);
 
-    
-   var radius,r;
+stroke(255);
+    fill(100);
+ rect(250,200,200,10);
+ fill(255,0,0);
+ ellipse(250,50,100,100);
+ stroke(0,255,0);
+ ellipse(450,50,100,100);
+ stroke(0,255,0);
+ 
+ }
+*/
 
-   function setup() {
-    createCanvas(500, 500);
+var radius,r;
+
+function setup(){
+    createCanvas(500,500);
+    background(255);
+radius=0;
+
+}
+
+function draw(){
+    radius++; // this is increasing by 1
+    fill(255,208,38)//colour
+    if(radius<200)// if the radius is less than 200
+    ellipse(250,250,radius,)//the circle will increase in size
+
+}
+else if(radius>=300){
     background(255)
-    radius = 0;
-    r = 600
+    ellipse(150,150,r-radius);
+    if (radius>=600){
+        radius=0
+    }
+}
+
+
